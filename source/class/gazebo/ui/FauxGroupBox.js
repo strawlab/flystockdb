dc744666f4572eea7a3b85783cc91bbeddd625df
Joachim Baran <joachim.baran@gmail.com>
2011-04-16 21:11:04 -0400

    Woops. Wrong comment-header. Replaced with correct one.

--- source/class/gazebo/ui/FauxGroupBox.js
+++ source/class/gazebo/ui/FauxGroupBox.js
@@ -1,30 +1,12 @@
-/* ************************************************************************
+/*
+   Copyright and License: see LICENSE file
 
-   qooxdoo - the new era of web development
-
-   http://qooxdoo.org
-
-   Copyright:
-     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
-
-   License:
-     LGPL: http://www.gnu.org/licenses/lgpl.html
-     EPL: http://www.eclipse.org/org/documents/epl-v10.php
-     See the LICENSE file in the project's top-level directory for details.
-
-   Authors:
-     * Sebastian Werner (wpbasti)
-     * Andreas Ecker (ecker)
-     * Martin Wittemann (martinwittemann)
-     * Jonathan Weiß (jonathan_rass)
-
-************************************************************************ */
+   Contributors:
+	   * Joachim Baran
+ */
 
 /**
- * Group boxes are used to group a set of form elements.
- *
- * @childControl frame {qx.ui.container.Composite} frame for the content widgets
- * @childControl legend {qx.ui.basic.Atom} legend to show at top of the groupbox
+ * A groupbox without legend.
  */
 qx.Class.define("gazebo.ui.FauxGroupBox",
 {
